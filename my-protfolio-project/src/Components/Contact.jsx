e975bcf7e20c8add482e6dce587b1149afc68ed5
unknown <emdrtad@gbm-015.gbm.net>
2024-01-05 14:31:35 +0100

    added minor changes

--- my-protfolio-project/src/Components/Contact.jsx
+++ my-protfolio-project/src/Components/Contact.jsx
@@ -3,23 +3,20 @@ import "../scss/Contact.scss";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Contact = () => {
-    return(
+    return (
         <div className="contact-container">
-            <h3>Feel free to reach out</h3>
+            <h3>Reach out</h3>
             <form>
-                <div>
-                    <input type="text" placeholder='Full name' />
-                    <input type="email" placeholder='Email' />
-                </div>
+                <input type="text" placeholder='Full name' />
+                <input type="email" placeholder='Email' />
 
                 <input type="text" placeholder='Subject' />
 
                 <textarea placeholder='Message'></textarea>
 
                 <button>
-                        SUBSCRIBE
-                        <FaLongArrowAltRight />   
-                    </button>
+                    SEND
+                </button>
             </form>
         </div>
     )
